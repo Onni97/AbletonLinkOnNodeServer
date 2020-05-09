26b8fd32c0305642f6cc492af9d03664b326a249
Alessandro Oniarti <onniale@gmail.com>
2020-05-09 15:55:01 +0200

    making device select

--- server.js
+++ server.js
@@ -2,11 +2,27 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+const fs = require('fs');
 
 app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/pages/deviceSelect.html')
+});
+
+app.get('/deviceSelectPC-Mobile', (req, res) => {
+    res.sendFile(__dirname + '/pages/deviceSelectPC-Mobile.html');
+});
+
+app.get('/link', (req, res) => {
+    fs.readFile(__dirname + '/pages/index.html', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            let page = data.toString().replace("LATECY_TO_REPLACE", "0");
+            res.send(page);
+        }
+    });
 });
 
 
@@ -23,16 +39,20 @@ const link = new AbletonLink();
 const io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    //console.log('A user connected');
+    socket.emit("numPeers", link.getNumPeers());
+    socket.emit("tempo", link.getTempo(true));
+    socket.emit("startStopSyncEnabled", link.isStartStopSyncEnabled());
+    socket.emit("beat", link.getBeat(true));
+    socket.emit("phase", link.getPhase(true));
+    socket.emit("latency", getDeviceLatency());
+    socket.emit("handshakeOK");
 
-    socket.on("handshake", (deviceID) => {
-        console.log(deviceID);
-        socket.emit("tempo", link.getTempo(true));
-        socket.emit("numPeers", link.getNumPeers());
-        socket.emit("startStopSyncEnabled", link.isStartStopSyncEnabled());
-    });
+    socket.on("print", (msg) => {
+        console.log(msg);
+    })
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        //console.log('A user disconnected');
     });
 });
 
@@ -41,8 +61,8 @@ link.setTempoCallback((tempo) => io.emit('tempo', link.getTempo(true)));
 link.setStartStopCallback((startStopState) => io.emit('playState', startStopState));
 
 setInterval(() => {
-    io.emit("beat", link.getBeat());
-    io.emit("phase", link.getPhase());
+    io.emit("beat", link.getBeat(true));
+    io.emit("phase", link.getPhase(true));
 }, 5);
 
 
@@ -54,5 +74,5 @@ let getDeviceLatency = function (deviceID) {
 
 
 http.listen(3000, () => {
-    console.log('listening on *:3000');
+    console.log('listening on port 3000');
 });

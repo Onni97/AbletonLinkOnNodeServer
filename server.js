f4774e8b2da76fb2b671ab354fbfbb9c9db426f1
Alessandro Oniarti <onniale@gmail.com>
2020-05-12 19:33:00 +0200

    going on with device selection

--- server.js
+++ server.js
@@ -14,25 +14,58 @@ app.get('/deviceSelectPC-Mobile', (req, res) => {
     res.sendFile(__dirname + '/pages/deviceSelectPC-Mobile.html');
 });
 
-app.get('/link', (req, res) => {
-    fs.readFile(__dirname + '/pages/index.html', function (err, data) {
+app.get('/deviceSelectVendor', (req, res) => {
+    fs.readFile(__dirname + '/pages/deviceSelectVendor.html', function (err, data) {
         if (err) {
             console.log(err);
         } else {
-            let page = data.toString().replace("LATECY_TO_REPLACE", "0");
+            let stringToInsert = "";
+            let keys = Object.keys(deviceLatencies);
+            keys.forEach(function (item, index) {
+                stringToInsert += '<div class="col colCard d-flex align-items-center">\n' +
+                    '        <div class="card mx-auto" style="height: 0; width: 0" onclick="clicked(\'' + item + '\')">\n' +
+                    '            <img class="card-img-top" src="/static/images/' + item + '.svg" alt="Card image cap">\n' +
+                    '            <div class="card-body">\n' +
+                    '                <h4 class="card-title">' + item + '</h4>\n' +
+                    '            </div>\n' +
+                    '        </div>\n' +
+                    '    </div>';
+            })
+            let page = data.toString().replace("TO_REPLACE", stringToInsert);
             res.send(page);
         }
     });
 });
 
+app.get('/deviceSelectModel', (req, res) => {
+    //TODO: risistemare il codice, in modo che ad ogni selezione si passino i dati e si salvino nella sessione
+});
 
+app.get('/link', (req, res) => {
+    //da impostare la latenza;
+    let latency = 0;
+    fs.readFile(__dirname + '/pages/link.html', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            let page = data.toString().replace("LATECY_TO_REPLACE", latency);
+            res.send(page);
+        }
+    });
+});
+
+app.get('/footer', (req, res) => {
+    res.sendFile(__dirname + "/pages/footer.html");
+});
+
+
+//load device-latencies file
+let deviceLatencies = require(__dirname + '/resources/device-latencies.json');
 
 
 //setup abletonlink-addon
 const AbletonLink = require("abletonlink-addon");
 const link = new AbletonLink();
-
-
 
 
 //setup socket.io
@@ -66,13 +99,12 @@ setInterval(() => {
 }, 5);
 
 
-
-
 let getDeviceLatency = function (deviceID) {
 
 }
 
 
+//start listening
 http.listen(3000, () => {
     console.log('listening on port 3000');
 });
